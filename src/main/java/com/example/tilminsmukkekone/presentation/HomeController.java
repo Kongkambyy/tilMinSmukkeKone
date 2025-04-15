438f8f5f58282373849f477f8bf606dd6d2301ea
kongkambyy <joachimkambyn@gmail.com>
2025-04-15 11:04:35 +0200

    Updated

--- src/main/java/com/example/tilminsmukkekone/presentation/HomeController.java
+++ src/main/java/com/example/tilminsmukkekone/presentation/HomeController.java
@@ -2,6 +2,8 @@ package com.example.tilminsmukkekone.presentation;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -9,5 +11,15 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "Body";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLogin(@RequestParam String username, @RequestParam String password) {
+        return "redirect:/";
     }
 }
